a9381e6450bfd773b3db96fedbbf88e2f8a3a403
Nikola Pantelic <pantelicnikolaa@gamail.com>
2025-01-15 14:24:03 +0100

    najposecenije stranice na sajtu

--- js/script.js
+++ js/script.js
@@ -22,44 +22,42 @@ const handleScroll = () => {
 window.addEventListener("scroll", handleScroll);
 
 document.addEventListener("DOMContentLoaded", () => {
-
-  const currentPage = window.location.pathname; 
-  const pageName = document.title || currentPage; 
+  const currentPage = window.location.pathname;
+  const pageName = document.title || currentPage;
 
   function logVisit(page, name) {
-    let visits = JSON.parse(localStorage.getItem("pageVisits")) || {}; 
+    let visits = JSON.parse(localStorage.getItem("pageVisits")) || {};
     if (!visits[page]) {
-      visits[page] = { count: 0, name }; 
+      visits[page] = { count: 0, name };
     }
-    visits[page].count += 1; 
-    localStorage.setItem("pageVisits", JSON.stringify(visits)); 
+    visits[page].count += 1;
+    localStorage.setItem("pageVisits", JSON.stringify(visits));
   }
 
-  logVisit(currentPage, pageName); 
-
-  const links = document.querySelectorAll("a"); 
-  links.forEach(link => {
-    link.addEventListener("click", (event) => {
-      const url = event.target.href;
-      const name = event.target.textContent.trim(); 
-      logClick(url, name); 
-    });
-  });
-
+  logVisit(currentPage, pageName);
 
   displayTopLinks();
 });
 
 function displayTopLinks() {
   const visits = JSON.parse(localStorage.getItem("pageVisits")) || {};
-  const sortedPages = Object.entries(visits)
-    .sort((a, b) => b[1].count - a[1].count) 
-    .slice(0, 3); 
+
+  const sortedPages = Object.entries(visits);
+
+  for (let i = 0; i < sortedPages.length - 1; i++) {
+    for (let j = i + 1; j < sortedPages.length; j++) {
+      if (sortedPages[i][1].count < sortedPages[j][1].count) {
+        const temp = sortedPages[i];
+        sortedPages[i] = sortedPages[j];
+        sortedPages[j] = temp;
+      }
+    }
+  }
 
   const ul = document.querySelector(".most-viewed-links");
-  sortedPages.forEach(([url, data]) => {
+  sortedPages.slice(0, 3).forEach(([url, data]) => {
     const li = document.createElement("li");
-    li.innerHTML = `<a href="${url}">${data.name}</a>`; 
+    li.innerHTML = `<a href="${url}">${data.name}</a>`;
     ul.appendChild(li);
   });
 }
